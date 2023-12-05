00bf7723e583c2c49d8ebde50c23ee7a89cee914
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2023-12-05 21:39:27 +0000

    fix: type of env name

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.REACT_APP_DB_URL,
+    connectionString: process.env.DB_URL,
     ssl: {
       rejectUnauthorized: false
     }
