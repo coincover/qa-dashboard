e650cf4733876a8dc102f09eed6f3edd1e5ce7d6
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2023-12-11 16:07:19 +0000

    refactor: updated date regex

--- server/validators/create-unit-schema.js
+++ server/validators/create-unit-schema.js
@@ -1,10 +1,10 @@
 const yup = require('yup');
 
-const DATE_REGEX = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/;
+const DATE_REGEX = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])_(0\d|1\d|2[0-3]):([0-5]\d):([0-5]\d))$/;
 
 const createUnitSchema = yup
   .object({
-    date: yup.string().matches(DATE_REGEX, 'date must be in the format YYYY-MM-DD'),
+    date: yup.string().matches(DATE_REGEX, 'date must be in the format YYYY-MM-DD_HH:MM:SS'),
     // percentage: yup.string().required(),
     commit: yup.string().required(),
     pull_request: yup.string().required(),
