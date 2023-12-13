aed2cb52d7ba79e6f03ca258c5581a8362d3450a
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2023-12-13 11:29:34 +0000

    bug-fix: loading issue due to bad commenting

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import storage from 'redux-persist/lib/storage';
 import { thunk } from 'redux-thunk';
 import rootReducer from './reducer';
@@ -13,7 +12,8 @@ const persistConfig = {
 const middleware = [thunk];
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composeEnhancers = process.env.NODE_ENV !== 'production' ? composeWithDevTools : compose;
+// Use compose from redux package
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(persistedReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
 const persistor = persistStore(store);
