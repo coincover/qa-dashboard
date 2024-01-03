f9f482303ab24225470fdc88acb043c715fcf698
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-01-03 09:32:50 +0000

    feat: added new endpoint for SDS

--- server/database/migrate.js
+++ server/database/migrate.js
@@ -1,137 +1,35 @@
 const db = require('./connection');
 
+const products = require('../utils/constant').products;
+
 (async () => {
   try {
-    // Drop existing tables
-    await db.schema.dropTableIfExists('e2e_disaster_recovery');
-    await db.schema.dropTableIfExists('unit_disaster_recovery');
-    await db.schema.dropTableIfExists('e2e_identity_service');
-    await db.schema.dropTableIfExists('unit_identity_service');
-    await db.schema.dropTableIfExists('e2e_ledger_support_tool');
-    await db.schema.dropTableIfExists('unit_ledger_support_tool');
-    await db.schema.dropTableIfExists('e2e_recovery_as_a_service');
-    await db.schema.dropTableIfExists('unit_recovery_as_a_service');
-    await db.schema.dropTableIfExists('e2e_transaction_protection');
-    await db.schema.dropTableIfExists('unit_transaction_protection');
-    await db.schema.dropTableIfExists('e2e_status');
-
-    // Create e2e_disaster_recovery table
-    await db.schema.createTable('e2e_disaster_recovery', (table) => {
-      table.increments('id').primary();
-      table.timestamp('timestamp');
-      table.integer('pass');
-      table.integer('fail');
-      table.integer('skip');
-    });
-
-    // Create unit_disaster_recovery table
-    await db.schema.createTable('unit_disaster_recovery', (table) => {
-      table.increments('id').primary();
-      table.timestamp('date');
-      table.string('percentage');
-      table.string('commit');
-      table.string('pull_request');
-      table.string('statement_coverage');
-      table.string('function_coverage');
-      table.string('branch_coverage');
-      table.string('line_coverage');
-      table.string('author');
-    });
-
-    // Create e2e_identity_service table
-    await db.schema.createTable('e2e_identity_service', (table) => {
-      table.increments('id').primary();
-      table.timestamp('timestamp');
-      table.integer('pass');
-      table.integer('fail');
-      table.integer('skip');
-    });
-
-    // Create unit_identity_service table
-    await db.schema.createTable('unit_identity_service', (table) => {
-      table.increments('id').primary();
-      table.timestamp('date');
-      table.string('percentage');
-      table.string('commit');
-      table.string('pull_request');
-      table.string('statement_coverage');
-      table.string('function_coverage');
-      table.string('branch_coverage');
-      table.string('line_coverage');
-      table.string('author');
-    });
-
-    // Create e2e_ledger_support_tool table
-    await db.schema.createTable('e2e_ledger_support_tool', (table) => {
-      table.increments('id').primary();
-      table.timestamp('timestamp');
-      table.integer('pass');
-      table.integer('fail');
-      table.integer('skip');
-    });
-
-    // Create unit_ledger_support_tool table
-    await db.schema.createTable('unit_ledger_support_tool', (table) => {
-      table.increments('id').primary();
-      table.timestamp('date');
-      table.string('percentage');
-      table.string('commit');
-      table.string('pull_request');
-      table.string('statement_coverage');
-      table.string('function_coverage');
-      table.string('branch_coverage');
-      table.string('line_coverage');
-      table.string('author');
-    });
-
-    // Create e2e_recovery_as_a_service table
-    await db.schema.createTable('e2e_recovery_as_a_service', (table) => {
-      table.increments('id').primary();
-      table.timestamp('timestamp');
-      table.integer('pass');
-      table.integer('fail');
-      table.integer('skip');
-    });
-
-    // Create unit_recovery_as_a_service table
-    await db.schema.createTable('unit_recovery_as_a_service', (table) => {
-      table.increments('id').primary();
-      table.timestamp('date');
-      table.string('percentage');
-      table.string('commit');
-      table.string('pull_request');
-      table.string('statement_coverage');
-      table.string('function_coverage');
-      table.string('branch_coverage');
-      table.string('line_coverage');
-      table.string('author');
-    });
-
-    // Create e2e_transaction_protection table
-    await db.schema.createTable('e2e_transaction_protection', (table) => {
-      table.increments('id').primary();
-      table.timestamp('timestamp');
-      table.integer('pass');
-      table.integer('fail');
-      table.integer('skip');
-    });
-
-    // Create unit_transaction_protection table
-    await db.schema.createTable('unit_transaction_protection', (table) => {
-      table.increments('id').primary();
-      table.timestamp('date');
-      table.string('percentage');
-      table.string('commit');
-      table.string('pull_request');
-      table.string('statement_coverage');
-      table.string('function_coverage');
-      table.string('branch_coverage');
-      table.string('line_coverage');
-      table.string('author');
-    });
+    for (const product of products) {
+      // Create e2e table
+      await db.schema.createTableIfNotExists(`e2e_${product}`, (table) => {
+        table.increments('id').primary();
+        table.timestamp('timestamp');
+        table.integer('pass');
+        table.integer('fail');
+        table.integer('skip');
+      });
+      // Create unit table
+      await db.schema.createTableIfNotExists(`unit_${product}`, (table) => {
+        table.increments('id').primary();
+        table.timestamp('date');
+        table.string('percentage');
+        table.string('commit');
+        table.string('pull_request');
+        table.string('statement_coverage');
+        table.string('function_coverage');
+        table.string('branch_coverage');
+        table.string('line_coverage');
+        table.string('author');
+      });
+    }
 
     // Create e2e_status table
-    await db.schema.createTable('e2e_status', (table) => {
+    await db.schema.createTableIfNotExists('e2e_status', (table) => {
       table.increments('id').primary();
       table.string('product'); // The name of the product
       table.string('status'); // Status for the product (pass/fail/unknown)
--- server/utils/constant.js
+++ server/utils/constant.js
@@ -1,5 +1,12 @@
 module.exports = {
-  products: ['disaster_recovery', 'identity_service', 'ledger_support_tool', 'recovery_as_a_service', 'transaction_protection'],
+  products: [
+    'disaster_recovery',
+    'identity_service',
+    'ledger_support_tool',
+    'recovery_as_a_service',
+    'transaction_protection',
+    'secure_data_service'
+  ],
   jira_instance: 'https://coincover.atlassian.net/',
   sonar_cloud_instance: 'https://sonarcloud.io'
 };
