072bcacfc4d4d66a9a33efb5b9db91a8deafd752
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-01-04 20:17:55 +0000

    fix: default value for state

--- src/store/e2eReducer.js
+++ src/store/e2eReducer.js
@@ -1,6 +1,13 @@
 import { GET_E2E } from './actions';
 
-const initialState = {};
+const initialState = {
+  transaction_protection: [],
+  disaster_recovery: [],
+  identity_service: [],
+  recovery_as_a_service: [],
+  secure_data_service: [],
+  ledger_support_tool: []
+};
 
 function e2eReducer(state = initialState, action) {
   const { type, payload } = action;
--- src/store/unitReducer.js
+++ src/store/unitReducer.js
@@ -1,6 +1,13 @@
 import { GET_UNIT } from './actions';
 
-const initialState = {};
+const initialState = {
+  transaction_protection: [],
+  disaster_recovery: [],
+  identity_service: [],
+  recovery_as_a_service: [],
+  secure_data_service: [],
+  ledger_support_tool: []
+};
 
 function unitReducer(state = initialState, action) {
   const { type, payload } = action;
