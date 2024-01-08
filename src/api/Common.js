f3e5de9d3d06acf9e99e632679cd201fe2f6cd3a
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-01-08 10:26:14 +0000

    debug: test

--- src/api/Common.js
+++ src/api/Common.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log('API Key:', process.env.REACT_APP_API_KEY); // For debugging purposes
+
 export default axios.create({
   baseURL: 'https://qa-dashboard-4gfp.onrender.com',
   headers: {
