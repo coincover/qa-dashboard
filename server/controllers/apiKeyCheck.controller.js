d5721f72a7a88f705c682c403efac9fa90715ce5
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-01-08 19:58:00 +0000

    revert api token test

--- server/controllers/apiKeyCheck.controller.js
+++ server/controllers/apiKeyCheck.controller.js
@@ -1,18 +1,5 @@
 const error401 = require('../errors/401.js');
 
-function generateRandomString(length) {
-  var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-process.env.REACT_APP_API_KEY = generateRandomString(10);
-console.log(process.env.REACT_APP_API_KEY);
-
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const apiKeyCheck = (req, res, next) => {
