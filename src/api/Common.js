6782c12a5bc36345a6b212e6466ae07c7b0ef9a1
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-01-08 10:41:14 +0000

    debug: test

--- src/api/Common.js
+++ src/api/Common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 console.log('API Key:', process.env.REACT_APP_API_KEY); // For debugging purposes
-console.log('SONAR_CLOUD Key: ', process.env.SONAR_CLOUD_TOKEN); // For debugging purposes
+console.log('SONAR_CLOUD Key: ', process.env.API_KEY); // For debugging purposes
 export default axios.create({
   baseURL: 'https://qa-dashboard-4gfp.onrender.com',
   headers: {
