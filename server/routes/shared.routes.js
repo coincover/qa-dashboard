08dc1bd89d28aee66fabc449b833727557cb6185
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-01-08 17:04:18 +0000

    refactor: remove api check for post req

--- server/routes/shared.routes.js
+++ server/routes/shared.routes.js
@@ -4,7 +4,7 @@ const { apiKeyCheck } = require('../controllers/apiKeyCheck.controller');
 const { getAllDataFromTable, createData, deleteDataRow } = require('../controllers/shared.controller');
 
 router.get('/:type/:product', apiKeyCheck, getAllDataFromTable);
-router.post('/:type/:product', apiKeyCheck, createData);
+router.post('/:type/:product', createData);
 router.delete('/:type/:product/:id', apiKeyCheck, deleteDataRow);
 
 module.exports = router;
